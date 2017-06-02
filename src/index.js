67804322ee6ecf0b5c832eacdeab4022ad599fc6
Anton <antonveryga@gmail.com>
2017-06-02 12:47:03 +0300

    Cavs as a default search

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ class App extends Component {
       selectedVideo: null
     };
 
-    this.videoSearch('surfboards');
+    this.videoSearch('cavs');
   }
   videoSearch(term){
     YTSearch({key: API_KEY, term: term}, (videos) => {
